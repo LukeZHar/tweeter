01ef854cab81038867b471b13f7faba597cf9af8
LukeZHar <luke.z.harris@hotmail.com>
2025-12-21 15:17:12 +1000

    chore/feat: add package.json with initial dependencies and scripts
    created basic auth controller and route
    Created a db connect

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signUp = (req, res) => {
+  res.json({
+    data: "SignUp route",
+  });
+};
+
+export const login = (req, res) => {
+  res.json({
+    data: "Login route",
+  });
+};
+
+export const logout = (req, res) => {
+  res.json({
+    data: "Logout route",
+  });
+};
