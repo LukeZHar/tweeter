506ab3bd8f1f4c712940ef1ca2218f770b8c310a
LukeZHar <luke.z.harris@hotmail.com>
2026-01-02 16:02:09 +1000

    feat: implement getSuggestedUsers functionality to provide user recommendations

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -15,8 +15,6 @@ export const getUserProfile = async (req, res) => {
   }
 };
 
-export const getSuggestedUsers = async (req, res) => {};
-
 export const followUnfollowUser = async (req, res) => {
   try {
     const { id } = req.params;
@@ -57,6 +55,33 @@ export const followUnfollowUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
     console.error("Error in follow/unfollow user:", error);
+  }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const usersFollowedByMe = await User.findById(userId).select("following");
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByMe.following.includes(user._id)
+    );
+    const suggestedUsers = filteredUsers.slice(0, 4);
+
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    console.log("Error fetching suggested users:", error);
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
