044dd55a589ae259deab7d4a08f18d6b4dee9dc5
LukeZHar <luke.z.harris@hotmail.com>
2026-01-03 14:29:32 +1000

    feat: implement post creation with routes and model - Created liking, commenting, and deletion functionality calls without functions yet

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,41 @@
+import User from "../models/user.model.js";
+import Post from "../models/post.model.js";
+import { v2 as cloudinary } from "cloudinary";
+
+export const createPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    let { img } = req.body;
+
+    const userId = req.user.id.toString();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (!text && !img) {
+      return res.status(400).json({ message: "Post cannot be empty" });
+    }
+
+    if (img) {
+      const uploadResponse = await cloudinary.uploader.upload(img);
+      img = uploadResponse.secure_url;
+    }
+
+    const newPost = new Post({
+      user: userId,
+      text,
+      img,
+    });
+
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const likeUnlikePost = async (req, res) => {};
+export const commentOnPost = async (req, res) => {};
+export const deletePost = async (req, res) => {};
