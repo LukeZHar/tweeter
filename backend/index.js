5c272f28fea702c288b619ab94a2811592f3de6a
LukeZHar <luke.z.harris@hotmail.com>
2026-01-10 15:45:21 +1000

    feat: refactor route imports and create separate route files for auth and posts

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ import cookieParser from "cookie-parser";
 import dbConnect from "./db/dbConnect.js";
 import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.routes.js";
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
 
