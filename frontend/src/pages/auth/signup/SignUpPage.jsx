ef95a9567392b149712432f93e7c32f3cba21cda
LukeZHar <luke.z.harris@hotmail.com>
2026-01-11 18:53:41 +1000

    feat: add error handling placeholder in SignUpPage component

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -28,6 +28,8 @@ const SignUpPage = () => {
         })
     };
 
+    const isError = false; // Placeholder for error state
+
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen px-10">
         <div className="flex-1 hidden lg:flex items-center justify-center">
@@ -86,7 +88,9 @@ const SignUpPage = () => {
                 <button className="btn rounded-full btn-primary text-white">
                     {/* {isPending ? "Loading..." : "Sign up"} */} Sign up
                 </button>
-                {/* {isError && <p className="text-red-500">{error.message}</p>} */}
+                {isError && <p className="text-red-500">
+                    {/* {error.message} */} Error occurred during sign up.
+                </p>}
             </form>
             <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
                 <p className="text-white text-lg">Already have an account?</p>
