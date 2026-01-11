0cdbadb568a2a8f87d845c0a290affa176163301
LukeZHar <luke.z.harris@hotmail.com>
2026-01-11 17:36:08 +1000

    feat: add routing and authentication pages, integrate Tailwind CSS and DaisyUI
    - Added react-router-dom for routing.
    - Created SignUpPage, LoginPage, and HomePage components.
    - Updated App component to include routes for the new pages.
    - Integrated Tailwind CSS and DaisyUI for styling.
    - Added PostCSS configuration for Tailwind.
    - Included various avatar and post images in the public directory.
    - Removed unused vite.svg file.
    - Updated vite.config.js to set server port to 3000.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import SignUpPage from "./pages/auth/signup/SignUpPage"
+import LoginPage from "./pages/auth/login/LoginPage"
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   )
 }
 
